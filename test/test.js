9291d8574278b9dba2dc77da11955cdda2f40646
Dan Hassin <danhassin@mac.com>
2016-09-04 18:20:24 -0400

    Use replaceMatching for HAML delimiter

--- test/test.js
+++ test/test.js
@@ -11,21 +11,27 @@ describe('Replace Matching', () => {
     let options = {open: '{', close: '}'}
     let replace
 
+    replace = HamlJSX.replaceMatching("nothing", options, () => 'hi');
+    assert.equal(replace, 'nothing');
+
     replace = HamlJSX.replaceMatching(string, options, () => 'hi');
     assert.equal(replace, 'xy: hi z: hi');
 
-    replace = HamlJSX.replaceMatching(string, options, (x) => x);
-    assert.equal(replace, string);
+    replace = HamlJSX.replaceMatching(string, options, (x) => '['+x+']');
+    assert.equal(replace, "xy: [a {b} {c}] z: [d {}]");
+
+    replace = HamlJSX.replaceMatching("xy: (~a {b} {c}~) z: (~d {}~)", {open: '(~', close: '~)'}, (x) => '['+x+']');
+    assert.equal(replace, "xy: [a {b} {c}] z: [d {}]");
 
     options.findRegex = /((\w+): ){/
     replace = HamlJSX.replaceMatching(string, options, (x, match) => match[2]);
     assert.equal(replace, "xy z");
 
     replace = HamlJSX.replaceMatching(string, options, (x, match) => x);
-    assert.equal(replace, "{a {b} {c}} {d {}}");
+    assert.equal(replace, "a {b} {c} d {}");
 
     replace = HamlJSX.replaceMatching(string, options, (x, match) => match[2]+'='+x);
-    assert.equal(replace, "xy={a {b} {c}} z={d {}}");
+    assert.equal(replace, "xy=a {b} {c} z=d {}");
   });
 });
 
